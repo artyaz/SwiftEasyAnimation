a380e0369b766bc02189eee2c9628f3fec7e148f
artyaz <achmilenko58@gmail.com>
2023-05-06 12:23:00 +0300

    Updated counter

--- Project15/ViewController.swift
+++ Project15/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var imageView: UIImageView!
+    var currentAnimation = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
     }
 
-
+    @IBAction func tapped(_ sender: UIButton) {
+        sender.isHidden = true
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 15, animations: {
+            switch self.currentAnimation {
+            case 0:
+                self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
+                break
+            default:
+                self.imageView.transform = .identity
+                break
+            }
+        })
+        {finished in
+            sender.isHidden = false
+        }
+        
+        currentAnimation += 1
+        
+        if currentAnimation > 1 {
+            currentAnimation = 0
+        }
+    }
+    
 }
 
